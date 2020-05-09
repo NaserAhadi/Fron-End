bf1fbf565ccc3ec4e2db1f5ccfb17d19de169cde
NaserAhadi <52449770+NaserAhadi@users.noreply.github.com>
2020-05-09 20:20:17 +0430

    jQuery - Selectors

---
+++ jQuery/jQuery-practice/Selectors/main.js
@@ -0,0 +1,20 @@
+// $('h1').hide();
+// $('h1#heading1').hide();
+// $('#heading1').hide();
+// $('h1.heading2').hide();
+// $('.heading2').hide();
+$('p span').css('color','green');
+
+$('ul#list li:first').css('color','red');
+$('ul#list li:last').css('color','blue');
+$('ul#list li:even').css('background-color','yellow');
+$('ul#list li:odd').css('background-color','#ccc');
+$('ul#list li:nth-child(2n)').css('list-style','none');
+
+// $(':button').hide();
+// $(':submit').hide();
+// $(':text').hide();
+
+$('[href]').css('color','red');
+$('a[href = "https://us.yahoo.com/"]').css('color','green');
+// $('*').hide();
