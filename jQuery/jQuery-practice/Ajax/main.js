06f4e2bf82b6e5a290848af88ebcd9550808a4cc
NaserAhadi <52449770+NaserAhadi@users.noreply.github.com>
2020-05-12 19:21:33 +0430

    jQuery - Ajax

--- jQuery/jQuery-practice/Ajax/main.js
+++ jQuery/jQuery-practice/Ajax/main.js
@@ -1,36 +1,4 @@
-/*
-AJAX is the art of exchanging data with a server, and updating parts of a web page
-- without reloading the whole page.
-
-AJAX = Asynchronous JavaScript and XML.
-
-In short; AJAX is about loading data in the background and display it on the webpage,
-without reloading the whole page.
-
-Examples of applications using AJAX: Gmail, Google Maps, Youtube, and Facebook tabs.
- */
-
-/*
-installing live server is a bit tricky:
-after installing node js, install live server manually,
-download live server from : https://github.com/tapio/live-server
-and go to the folder that you have downloaded and open terminal,
-type this command : sudo npm install -g
-
-There are two reasons for using live server:
-1-AJAX requests don't work with the file:// protocol due to security restrictions, i.e.
-you need a server if your site fetches content through JavaScript.
-
-2-Having the page reload automatically after changes to files can accelerate development.
- */
 $(document).ready(function () {
-/*
-$(selector).load(URL,data,callback);
-The callback function can have different parameters:
-responseTxt - contains the resulting content if the call succeeds
-statusTxt - contains the status of the call
-xhr - contains the XMLHttpRequest object
-*/
     // $('#result').load('test.html');
 
     $('#result').load('test1.html', function (responseTxt, statusTxt, xhr) {
